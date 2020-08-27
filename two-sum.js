7dce29ce3041f97377600e5ae9f482dd3a3631b3
lucas1004jx <lucas1004jx@gmail.com>
2020-08-27 23:19:56 +0200

    two sum hash map solution

--- two-sum.js
+++ two-sum.js
@@ -11,7 +11,7 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 */
 
-const twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
     
     for(let i=0; i<nums.length-1;i++){
        for(let n = i+1; n<nums.length;n++){
@@ -21,3 +21,28 @@ const twoSum = function(nums, target) {
     }
     
 };
+
+const twoSum2 = (nums, target) => {
+    for(let i=0; i < nums.length; i++){
+        const currentNum = nums[i];
+        const diff = target - currentNum;
+        const diffIdx = nums.findIndex(num => num === diff);
+        if(diffIdx !== i && diffIdx !== -1) return[i,diffIdx];
+    }
+}
+
+
+const twoSum3 = (nums, target) => {
+    const hashMap = new Map();
+    for(let i=0; i<nums.length; i++){
+        const current= nums[i];
+        if(hashMap.has(current)) return [hashMap.get(current), i];
+        const diff = target - nums[i];
+        hashMap.set(diff,i); 
+    }
+}
+
+const arr = [2,7,11,15];
+const target = 9;
+const r = twoSum3(arr,target);
+console.log('r-->',r);
